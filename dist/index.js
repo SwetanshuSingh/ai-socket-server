6a07bdc32cd1d8c497b4a3a664ca5dadd8e1a26b
SwetanshuSingh <swetanshusingh17@gmail.com>
2025-09-24 14:29:35 +0530

    feat: socket connection handler and user map

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,36 @@
-console.log("Hello World");
-export {};
+import { randomUUID } from "crypto";
+import http from "http";
+import url from "url";
+import { WebSocketServer, WebSocket } from "ws";
+const server = http.createServer();
+const users = new Map();
+const wss = new WebSocketServer({ noServer: true });
+wss.on("connection", (connection, request) => {
+    if (!request.url)
+        return;
+    const { username } = url.parse(request.url, true).query;
+    if (!username || typeof username !== "string")
+        return;
+    const uuid = randomUUID();
+    connection.socketId = uuid;
+    users.set(uuid, username);
+    // handle socket message events
+    connection.on("message", (data) => {
+        const parsedData = JSON.parse(data);
+        console.log(`This message is from ${connection.socketId}\nMessage - ${parsedData?.body}`);
+        console.log(`Connected Clients`, users);
+    });
+    // handle socket disconnection
+    connection.on("close", () => {
+        users.delete(connection.socketId);
+        console.log(`Connected Clients`, users);
+    });
+});
+server.on("upgrade", (request, socket, head) => {
+    wss.handleUpgrade(request, socket, head, (ws) => {
+        wss.emit("connection", ws, request);
+    });
+});
+server.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,56 @@
-console.log("Hello World")
+import { randomUUID } from "crypto";
+import http from "http";
+import url from "url";
+import { WebSocketServer, WebSocket } from "ws";
+
+const server = http.createServer();
+
+interface WebSocketConnection extends WebSocket {
+  socketId: string;
+}
+
+type Users = Map<string, string>;
+const users = new Map();
+
+const wss = new WebSocketServer({ noServer: true });
+
+wss.on("connection", (connection: WebSocketConnection, request) => {
+  if (!request.url) return;
+
+  const { username } = url.parse(request.url, true).query;
+
+  if (!username || typeof username !== "string") return;
+
+  const uuid = randomUUID();
+  connection.socketId = uuid;
+
+  users.set(uuid, username);
+
+  // handle socket message events
+  connection.on("message", (data: string) => {
+    const parsedData = JSON.parse(data);
+
+    console.log(
+      `This message is from ${connection.socketId}\nMessage - ${parsedData?.body}`
+    );
+
+    console.log(`Connected Clients`, users);
+  });
+
+  // handle socket disconnection
+  connection.on("close", () => {
+    users.delete(connection.socketId);
+
+    console.log(`Connected Clients`, users);
+  });
+});
+
+server.on("upgrade", (request, socket, head) => {
+  wss.handleUpgrade(request, socket, head, (ws) => {
+    wss.emit("connection", ws, request);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
